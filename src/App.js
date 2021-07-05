de3858e9c33e7aa4ce9f14f920b700c7e2f6d265
Scotlin White <scotlin23@hotmail.com>
2021-07-05 12:01:28 -0700

    react-vending-machine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, NavLink } from "react-router-dom";
+import "./App.css";
+import VendingMachine from "./VendingMachine";
+import Sardines from "./Sardines";
+import Soda from "./Soda";
+import Chips from "./Chips";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav className="App-nav">
+        <NavLink exact activeClassName="active-link" className="link" to="/">
+          VENDING MACHINE
+        </NavLink>
+        <NavLink
+          exact
+          activeClassName="active-link"
+          className="link"
+          to="/soda"
         >
-          Learn React
-        </a>
-      </header>
+          SODA
+        </NavLink>
+        <NavLink
+          exact
+          activeClassName="active-link"
+          className="link"
+          to="/chips"
+        >
+          CHIPS
+        </NavLink>
+        <NavLink
+          exact
+          activeClassName="active-link"
+          className="link"
+          to="/sardines"
+        >
+          SARDINES
+        </NavLink>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={VendingMachine} />
+        <Route exact path="/sardines" component={Sardines} />
+        <Route exact path="/soda" component={Soda} />
+        <Route exact path="/chips" component={Chips} />
+      </Switch>
     </div>
   );
 }
